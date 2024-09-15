f8a487423a120f1b93684a1949fe7614e0ae33a0
junghyun397 <junghyun397@gmail.com>
2024-09-15 23:49:10 +0900

    feat: init mintaka_webui and move mintaka_webui crate to mintaka_server

---
+++ mintaka_webui/src/Index.tsx
@@ -0,0 +1,8 @@
+import {render} from "solid-js/web"
+import "./index.css"
+
+function WebUiFrame() {
+    return <div>mintaka WebUI</div>
+}
+
+render(() => <WebUiFrame />, document.getElementById("app")!)
---
+++ mintaka_webui/tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: "selector",
+    content: ["./src/**/*.{ts,tsx,html}"],
+    plugins: [],
+}
